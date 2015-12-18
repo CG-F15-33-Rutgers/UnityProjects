fefc1182fe31d129aa34b5ba8a97abc0bfb21e75
napilla <pinilla.nicholas@gmail.com>
2015-12-17 19:08:22 -0500

    Added in KADAPT, simple behavior tree into the level

---
+++ BAssignments/B3/B3/Assets/Scripts/OverworldCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class OverworldCamera : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+        // just hardcode this for now I guess
+        transform.LookAt(new Vector3(10, 50, 50));
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        transform.position = transform.position + new Vector3(moveHorizontal, 0, moveVertical);
+
+        
+	}
+}
